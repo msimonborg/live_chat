6a812c9581f6c02e94223353cbb5b688e3a51a5d
M. Simon Borg <msimonborg@gmail.com>
2022-03-20 02:06:58 -0400

    finish heroku setup

--- lib/live_chat_web/endpoint.ex
+++ lib/live_chat_web/endpoint.ex
@@ -10,7 +10,8 @@ defmodule LiveChatWeb.Endpoint do
     signing_salt: "bE6QnF0Z"
   ]
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000, connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
